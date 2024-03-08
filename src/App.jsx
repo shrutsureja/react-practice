fcbc71581bd20a05f6144dc13feb0ae930f518ae
shrutsureja <shrutsureja.code@gmail.com>
2024-03-08 15:39:36 +0530

    Completed the thinking in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
 import { useState } from 'react'
 import './App.css'
+import ThinkingInReact from './components/ThinkingInReact'
 
 function App() {
 
   return (
-    <>
-      <p>Shrut Sureja</p>
+    <> 
+    <ThinkingInReact/>
     </>
   )
 }
 
 export default App
+
+/* 
+  - what to do and how ????
+  - lets build a small application while learning react
+  
+  - what should be the application ?
+  => As a end product lets build the paytm application
+
+  Next week deadline before saturday 
+  
+  College
+    - Maze project at least playable if not the CSS 
+  
+  Work
+    - Learn React and make a small project like paytm like application with concepts
+      - Context API
+      - prop drilling
+      - useEffect, useRef, and other hooks
+        -> Sources
+          - React.dev
+          - harkirat lectures 
+
+    - Learn NextJS for the frontend at least if not all then
+
+
+  Common things 
+  - react basics at least till the part of the context drilling/ prop drilling
+    using the simple hooks i will make mistakes in making but i will have to 
+    make it as soon as possible 
+    
+  - Conclusion 
+    i can learn react with the basics and then move to the Maze project building 
+    with the concepts that i have in hand. 
+    After that i should make a react Router poject like the paytm application 
+    and then move to the nextJS and learn it build the medicus application or try to make 
+    another simple application for the practice purpose at the same time
+
+    i need to work with backend of the project and learn more on the session, i can impleament
+    the session in the paytm project and then later move to the nextjs application
+
+
+    
+
+*/
